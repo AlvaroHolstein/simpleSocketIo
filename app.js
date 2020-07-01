7212032bd9df76a922d0c66089f43d2252838838
hols <adholstein@gmail.com>
2020-07-01 22:49:00 +0100

    playble server

--- app.js
+++ app.js
@@ -4,51 +4,103 @@ const io = require("socket.io")(httpserver);
 const jzz = require("jzz");
 const port = 3001;
 
-let midiIn = jzz().openMidiIn(function(){ return [2, 1, 0]; });
+let midiIn = jzz().openMidiIn(function () { return [2, 1, 0]; });
 let midiOut = jzz().openMidiOut();
 
+/** Primeiro "saber" se é possivel aceder ao midi do dispositivo */
 jzz.requestMIDIAccess()
-.then(() => {
-    console.log("Success on MIDI Access");
-})
-,() => console.error('Fail MIDI Access!!!!')
+    .then(() => {
+        console.log("Success on MIDI Access");
+        io.on("connection", mainSocketFunction);
+    })
+    , () => console.error('Fail MIDI Access!!!!')
 
+
+/** Dá para fazer isto de duas/tres maneiras
+ * io.sockets.clients().lenght, mas (https://stackoverflow.com/questions/10275667/socket-io-connected-user-count)
+ * por um contador (é o que está atualmente)
+ * por um array local, onde a cada connection dá se push para o array e a cada disconnction faz se o splice dessa disconnection
+ */
+let totalConnectedUsers = 0;
+
+
+/** Função chamada pelo socket */
+function mainSocketFunction(socket) {
+    //"Pingar"
+    midiOut.send(jzz.MIDI([0x90, 'Eb2', 127]))
+    totalConnectedUsers++;
+
+    for(let cli in io.sockets.clients()) {
+        // console.log(cli, io.sockets.clients().connected)
+    }
+    console.log("Entrou o " + totalConnectedUsers + "º"/*, io.sockets.clients().connected*/);
+    socket.on("join", (data) => {
+        console.log("Entrou o campeão " + data+ ", já são " + totalConnectedUsers)
+    })
+
+    socket.on("musicnote", receiveAndPlay)
+
+    /** Vai haver um socket listener para cada instrumento */
+    socket.on("drumm", receiveAndPlay)
+
+
+    /** A "flag" bradcast faz com que o emit vá para toda a gente menos para quem fez o "pedido" */
+    socket.broadcast.emit('newConnection', "New Connection!!!!")
+
+    /** Mandar para toda a gente quantos users estão disponiveis no momento */
+    socket.emit("totalUsers", totalConnectedUsers);
+
+
+    socket.on("disconnect", (data) => {
+        if(totalConnectedUsers>0) totalConnectedUsers--;
+        console.log("Menos Um!!!", totalConnectedUsers);
+    })
+}
+
+
+function receiveAndPlay(data) {
+    let value = data.split(':');
+    console.log(value, data);
+    if (value[0] > 61) value[0] = 61
+    if (value[0] < 0) value[0] = 0
+    midiOut.send([0x90, value[0], 74]);
+}
+
+/** Para ter mais opções de teste */
 server.get("/", (req, res) => {
     res.send("ATRa")
 })
-
-// kinda que estava a funcionar
 server.get("/tocar", async (req, res) => {
     await midiOut.or('Cannot open MIDI Out port!')
-    .wait(500).send(jzz.MIDI([0x90, 'Eb5', 127]))
-    .wait(500).send(jzz.MIDI([0x90,64,127]))
-    .wait(500).send([0x90,67,127])
-    .wait(500).send([0x90,72,127])
-    .wait(1000).send([0x90,60,0]).send([0x90,64,0]).send([0x90,67,0])
-    .send([0x90,72,0])
-    .and('thank you!')
+        .wait(500).send(jzz.MIDI([0x90, 'Eb5', 127]))
+        .wait(500).send(jzz.MIDI([0x90, 64, 127]))
+        .wait(500).send([0x90, 67, 127])
+        .wait(500).send([0x90, 72, 127])
+        .wait(1000).send([0x90, 60, 0]).send([0x90, 64, 0]).send([0x90, 67, 0])
+        .send([0x90, 72, 0])
+        .and('thank you!')
 
     res.send("Finito")
 })
-
-io.on("connection", (socket) => {
-    //"Pingar"
-    midiOut.send(jzz.MIDI([0x90, 'Eb5', 127]))
-
-    socket.on("musicnote", (data) => {
-        let value = data.split(':');
-        console.log(value, data);
-        if(value[0] > 61) value[0] = 61
-        if(value[0] < 0) value[0] = 0
-        midiOut.send([0x90, value[0], 74]);
-    })
+/** Uma maneira de termos todos os users sem ter que estar dentro do PlayPerfomance */
+server.get("/totalUsers", (req, res) => {
+    res.send(totalConnectedUsers);
 })
-
+/** Fechar o concerto basicamente */
 server.get("/close", async (req, res) => {
     console.log("a fechar");
     await jzz.close()
     res.send("closed")
 })
+
+
+
 httpserver.listen(port, () => {
+    midiOut.or('Cannot open MIDI Out port!')
+        .wait(500).send(jzz.MIDI([0x90, 'Eb5', 60]))
+
+    setTimeout(() => {
+        midiOut.allNotesOff(0);
+    }, 1000) // Serve para não deixar o som prelongar se demais
     console.log("A ouvir \n port: " + port + "\n");
 })
---
+++ selfplay.js
@@ -0,0 +1,38 @@
+const sc = require("socket.io-client")('http://localhost:3001');
+
+sc.emit("join", "almeida");
+sc.on('newConnection', (data) => {
+    console.log(data)
+})
+
+setInterval(() => {
+    sc.emit('musicnote', "30:50")
+}, 6000);
+setInterval(() => {
+    sc.emit('musicnote', '60:60')
+}, 3000);
+setInterval(() => {
+    sc.emit('musicnote', '80:80')
+}, 1000);
+
+
+
+https://stackoverflow.com/questions/10021373/what-is-the-windows-equivalent-of-process-onsigint-in-node-js
+if (process.platform === "win32") {
+    var rl = require("readline").createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+  
+    rl.on("SIGINT", function () {
+      process.emit("SIGINT");
+    });
+  }
+  
+  process.on("SIGINT", function () {
+    //graceful shutdown
+    sc.emit("disconnect")
+    console.error("sh")
+    process.exit();
+  });
+
